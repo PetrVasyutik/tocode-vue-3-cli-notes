981d127e368fa9168c2937865ebe5c5b44e39320
Petr <vasutik@mail.ru>
2024-12-31 11:51:36 +0300

    added form

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,3 +1,16 @@
 <template>
-  <p>hello home</p>
+  <mainForm @onSubmit="handleSubmit" />
 </template>
+
+<script>
+import mainForm from '@/components/Notes/mainForm.vue';
+
+export default {
+  components: { mainForm},
+  methods: {
+    handleSubmit (note) {
+      console.log(note)
+    }
+  }
+}
+</script>
